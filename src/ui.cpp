0ea24814d3dad87c75eb985a74fcaf33b2a005e3
kachhy <109984629+kachhy@users.noreply.github.com>
2026-02-02 15:29:21 -0500

    Basic UI services

--- src/ui.cpp
+++ src/ui.cpp
@@ -28,26 +28,133 @@ char _getch() {
 
 UIInstance* UIInstance::single = nullptr;
 
-void Button::render(
-	const size_t x,
-	const size_t y,
-	const size_t w,
-	const size_t h,
-	const Color base_color,
-	const Color selected_color,
-	const Color text_color,
-	const std::string& text
-) {
+void Button::render() const {
+	if (w < 2 || h < 2)
+		return;
+
+	drawSquare(x, y, w, h, outline_color);
+	drawSquare(x + 1, y + 1, w - 2, h - 2, active() ? selected_color : default_color);
+	if (!text.empty()) {
+		const size_t text_x = x * 2 + w - text.size() / 2;
+		EngineInstance::getInstance()->drawText(text_x, y + h / 2, text, text_color, active() ? selected_color : default_color);
+	}
+}
+
+void Button::onSelect() {
+	if (func != nullptr)
+		func();
+}
+
+void Slider::render() const {
 	if (w < 2 || h < 2)
 		return;
 
 	EngineInstance* inst = EngineInstance::getInstance();
-	const size_t text_x = x + w / 2 - text.size() / 2;
 
-	drawSquare(x, y, w, h, active() ? base_color : selected_color);
-	drawSquare(x + 1, y + 1, w - 2, h - 2, active() ? selected_color : base_color);
-	if (!text.empty())
-		inst->drawText(text_x, y + h / 2, text, text_color, active() ? selected_color : base_color);
+	// Bbox
+	drawSquare(x, y, w, h, outline_color);
+	drawSquare(x + 1, y + 1, w - 2, h - 2, background_color);
+
+	// Slider
+	const size_t slider_x = 1 + x + static_cast<double>(w - 3) * (static_cast<double>(cur_val - min_val) / static_cast<double>(max_val - min_val));
+	drawSquare(slider_x, y + 1, 1, h - 2, active() ? selected_slider_color : slider_color);
+}
+
+void Slider::onUpArrow() {
+	cur_val += step;
+	if (cur_val > max_val)
+		cur_val = max_val;
+
+	render();
+	EngineInstance::getInstance()->tick();
+}
+
+void Slider::onDownArrow() {
+	if (step >= cur_val - min_val)
+		cur_val = min_val;
+	else
+		cur_val -= step;
+
+	render();
+	EngineInstance::getInstance()->tick();
+}
+
+void CheckBox::render() const {
+	if (size < 3)
+		return;
+
+	drawSquare(x, y, size, size, outline_color);
+	drawSquare(x + 1, y + 1, size - 2, size - 2, is_selected ? selected_color : unselected_color);
+	
+	if (!text.empty()) {
+		const size_t text_x = 2 * (x + size + 1);
+		EngineInstance::getInstance()->drawText(text_x, y + size / 2, text, text_color, active() ? selected_color : unselected_color);
+	}
+}
+
+void CheckBox::onSelect() {
+	is_selected = !is_selected;
+	render();
+	EngineInstance::getInstance()->tick();
+}
+
+void ScrollableCanvas::render() const {
+	if (w < 3 || h < 3 || screen_w < 3 || screen_h < 3)
+		return;
+
+	// Render outline
+	drawBox(x, y, w + 1, h + 1, active() ? selected_color : outline_color);
+	drawSquare(x + 1, y + 1, w, h, Color::Default); // Clear background (TODO: maybe make this cooler and whatnot)
+
+	// Render elements on screen
+	for (const Task& task : screen) {
+		if (task.rule == RenderRule::RENDER_EMPTY)
+			continue;
+		if (task.x < view_x || task.x - view_x >= w
+			|| task.y < view_y || task.y - view_y >= h)
+			continue;
+		
+		Task task_repositioned(task);
+		task_repositioned.x = task.x + x + 1 - view_x;
+		task_repositioned.y = task.y + y + 1 - view_y;
+		EngineInstance::getInstance()->addToQueue(task_repositioned);
+	}
+}
+
+void ScrollableCanvas::onLeftArrow() {
+	if (static_cast<ssize_t>(view_x) - 1 < 0)
+		return;
+
+	--view_x;
+	render();
+	EngineInstance::getInstance()->tick();
+}
+
+void ScrollableCanvas::onRightArrow() {
+	if (view_x + w + 1 >= screen_w)
+		return;
+
+	++view_x;
+	render();
+	EngineInstance::getInstance()->tick();
+}
+
+void ScrollableCanvas::onUpArrow() {
+	if (static_cast<ssize_t>(view_y) - 1 < 0)
+		return;
+
+	--view_y;
+	render();
+	EngineInstance::getInstance()->tick();
+}
+
+void ScrollableCanvas::onDownArrow() {
+	if (view_y + w + 1 >= screen_h)
+		return;
+
+	++view_y;
+	render();
+	EngineInstance::getInstance()->tick();
 }
 
 bool UIInstance::inputTick() {
@@ -57,12 +164,44 @@ bool UIInstance::inputTick() {
 	char c = _getch();
 
 	if (c == '\t') {
-		elements[current_index].setActive(false);
+		elements[current_index]->setActive(false);
 		current_index++;
 		current_index %= elements.size();
-		elements[current_index].setActive(true);
+		elements[current_index]->setActive(true);
 		return true;
 	}
+	else if (c == '\n')
+		elements[current_index]->onSelect();
+#ifdef WIN32
+	else if (c == 72)
+		elements[current_index]->onUpArrow();
+	else if (c == 80)
+		elements[current_index]->onDownArrow();
+	else if (c == 75)
+		elements[current_index]->onLeftArrow();
+	else if (c == 77)
+		elements[current_index]->onRightArrow();
+#else
+	else if (c == 27) {
+		_getch(); // Skip [
+		c = _getch();
+		if (c == 'A')
+			elements[current_index]->onUpArrow();
+		else if (c == 'B')
+			elements[current_index]->onDownArrow();
+		else if (c == 'D')
+			elements[current_index]->onLeftArrow();
+		else if (c == 'C')
+			elements[current_index]->onRightArrow();
+	}
+#endif
 
 	return false;
+}
+
+void UIInstance::UITick() {
+	if (inputTick()) {
+		for (auto element : elements)
+			element->render();
+	}
 }
--- src/ui.h
+++ src/ui.h
@@ -4,27 +4,92 @@
 #include "draw.h"
 
 class UIElement {
-private:
+protected:
 	bool is_active;
 public:
 	UIElement() : is_active(false) { }
 	bool active() const { return is_active; };
 	void setActive(const bool val) { is_active = val; }
+	
+	virtual void render() const = 0;
+	
+	// Input functions
+	virtual void onSelect() { };
+	virtual void onLeftArrow() { };
+	virtual void onRightArrow() { };
+	virtual void onUpArrow() { };
+	virtual void onDownArrow() { };
 };
 
 class Button : public UIElement {
+protected:
+	std::string text;
+	void (*func)();
+	size_t x, y, w, h;
+	Color text_color, default_color, selected_color, outline_color;
+	bool is_active;
 public:
-	Button() = default;
-	void render(
-		const size_t x,
-		const size_t y,
-		const size_t w,
-		const size_t h,
-		const Color base_color,
-		const Color face_color,
-		const Color text_color,
-		const std::string& text = ""
-	);
+	Button(size_t x, size_t y, size_t w, size_t h, const std::string& text, void (*func)(), Color text_color, Color default_color, Color selected_color, Color outline_color) :
+		x(x), y(y), w(w), h(h), text(text), func(func), text_color(text_color), default_color(default_color), selected_color(selected_color), outline_color(outline_color) { }
+
+	void render() const override;
+	void onSelect() override;
+};
+
+class Slider : public UIElement {
+protected:
+	size_t x, y, w, h;
+	size_t min_val, cur_val, max_val, step; // Slider elements
+	Color outline_color, background_color, slider_color, selected_slider_color;
+	bool is_active;
+public:
+	Slider(size_t x, size_t y, size_t w, size_t h, size_t min_val, size_t max_val, size_t default_val, size_t step, Color outline_color, Color background_color, Color slider_color, Color selected_slider_color) : 
+		x(x), y(y), w(w), h(h), min_val(min_val), max_val(max_val), cur_val(default_val), step(step), outline_color(outline_color), background_color(background_color), slider_color(slider_color), selected_slider_color(selected_slider_color) { }
+
+	void render() const override;
+	void onUpArrow() override;
+	void onDownArrow() override;
+};
+
+class CheckBox : public UIElement {
+protected:
+	size_t x, y, size;
+	std::string text;
+	bool is_active, is_selected;
+	Color outline_color, unselected_color, selected_color, text_color; 
+public:
+	CheckBox(size_t x, size_t y, size_t size, const std::string& text, Color outline_color, Color unselected_color, Color selected_color, Color text_color, bool starting_value = false) :
+		x(x), y(y), size(size), text(text), outline_color(outline_color), unselected_color(unselected_color), selected_color(selected_color), text_color(text_color), is_selected(starting_value) { }
+
+	void render() const override;
+	void onSelect() override;
+	bool getSelected() const { return is_selected; }
+};
+
+class ScrollableCanvas : public UIElement {
+protected:
+	size_t x, y, w, h, screen_w, screen_h, view_x, view_y;
+	Color outline_color, selected_color;
+	std::vector<Task> screen;
+	bool is_active;
+public:
+	ScrollableCanvas(size_t x, size_t y, size_t w, size_t h, size_t screen_w, size_t screen_h, Color outline_color, Color selected_color) :
+		x(x), y(y), w(w), h(h), screen_w(screen_w), screen_h(screen_h), outline_color(outline_color), selected_color(selected_color), view_x(0), view_y(0)
+	{
+		screen.reserve(screen_w * screen_h);
+		for (size_t i = 0; i < screen_h; ++i) {
+			for (size_t j = 0; j < screen_w; ++j)
+				screen.emplace_back(j, i, Color::Default, RenderRule::RENDER_EMPTY);
+		}
+	}
+
+	void addToQueue(const Task& task) { screen[task.x + task.y * screen_w] = task; }
+	void render() const override;
+
+	void onLeftArrow() override;
+	void onRightArrow() override;
+	void onUpArrow() override;
+	void onDownArrow() override;
 };
 
 class UIInstance {
@@ -33,7 +98,7 @@ private:
 	static UIInstance* single;
 	size_t current_index;
 public:
-	std::vector<UIElement> elements; // TODO: temp
+	std::vector<UIElement*> elements;
 
 	[[nodiscard]] static UIInstance* getInstance() {
 		if (!single)
@@ -43,6 +108,7 @@ public:
 	}
 
 	bool inputTick();
+	void UITick();
 };
 
 #endif // UI_H
