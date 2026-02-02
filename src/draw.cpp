4dff3c2d460697cbc23413d124a244e5f04375c2
kachhy <109984629+kachhy@users.noreply.github.com>
2026-02-02 15:36:49 -0500

    Queue service function pointer for custom canvas

--- src/draw.cpp
+++ src/draw.cpp
@@ -1,6 +1,6 @@
 #include "draw.h"
 
-void drawLine(const size_t x_0, const size_t y_0, const size_t x_1, const size_t y_1, const Color color, const RenderRule rule) {
+void drawLine(const size_t x_0, const size_t y_0, const size_t x_1, const size_t y_1, const Color color, const RenderRule rule, void (*addToQueueService)(const Task&)) {
 	const double distance =
 		std::sqrt(std::pow(static_cast<int64_t>(x_0) - static_cast<int64_t>(x_1), 2) + std::pow(static_cast<int64_t>(y_0) - static_cast<int64_t>(y_1), 2));
 	const double dx = (static_cast<double>(x_1) - static_cast<double>(x_0)) / distance;
@@ -10,28 +10,31 @@ void drawLine(const size_t x_0, const size_t y_0, const size_t x_1, const size_t
 	double cy = y_0;
 
 	for (size_t i = 0; i < static_cast<size_t>(distance); ++i) {
-		EngineInstance::getInstance()->addToQueue(std::round(cx), std::round(cy), color, rule);
+		!!addToQueueService ? addToQueueService(Task(std::round(cx), std::round(cy), color, rule))
+						    : EngineInstance::getInstance()->addToQueue(std::round(cx), std::round(cy), color, rule);
 		cx += dx;
 		cy += dy;
 	}
 }
 
-void drawSquare(size_t x, size_t y, const size_t w, const size_t h, const Color color, const RenderRule rule) {
+void drawSquare(size_t x, size_t y, const size_t w, const size_t h, const Color color, const RenderRule rule, void (*addToQueueService)(const Task&)) {
 	for (int64_t i = y; i < y + h; ++i)
 		for (int64_t j = x; j < x + w; ++j)
-			EngineInstance::getInstance()->addToQueue(j, i, color, rule);
+			!!addToQueueService ? addToQueueService(Task(j, i, color, rule))
+								: EngineInstance::getInstance()->addToQueue(j, i, color, rule);
 }
 
-void drawBox(size_t x, size_t y, const size_t w, const size_t h, const Color color, const RenderRule rule) {
-	drawLine(x, y, x + w, y, color, rule);
-	drawLine(x, y, x, y + h, color, rule);
-	drawLine(x + w, y, x + w, y + h + 1, color, rule);
-	drawLine(x, y + h, x + w + 1, y + h, color, rule);
+void drawBox(size_t x, size_t y, const size_t w, const size_t h, const Color color, const RenderRule rule, void (*addToQueueService)(const Task&)) {
+	drawLine(x, y, x + w, y, color, rule, addToQueueService);
+	drawLine(x, y, x, y + h, color, rule, addToQueueService);
+	drawLine(x + w, y, x + w, y + h + 1, color, rule, addToQueueService);
+	drawLine(x, y + h, x + w + 1, y + h, color, rule, addToQueueService);
 }
 
-void drawCircle(const size_t x_0, const size_t y_0, const int r, const Color color, const RenderRule rule) {
+void drawCircle(const size_t x_0, const size_t y_0, const int r, const Color color, const RenderRule rule, void (*addToQueueService)(const Task&)) {
 	for (int64_t y = -r; y <= r; ++y)
 		for (int64_t x = -r; x <= r; ++x)
 			if (x * x + y * y <= r * r)
-				EngineInstance::getInstance()->addToQueue(static_cast<int64_t>(x_0) + x, static_cast<int64_t>(y_0) + y, color, rule);
+				!!addToQueueService ? addToQueueService(Task(static_cast<int64_t>(x_0) + x, static_cast<int64_t>(y_0) + y, color, rule))
+									: EngineInstance::getInstance()->addToQueue(static_cast<int64_t>(x_0) + x, static_cast<int64_t>(y_0) + y, color, rule);
 }
--- src/draw.h
+++ src/draw.h
@@ -4,9 +4,9 @@
 #include "engine.h"
 
 // Drawing tools
-void drawLine(const size_t x_0, const size_t y_0, const size_t x_1, const size_t y_1, const Color color, const RenderRule rule = RenderRule::RENDER_DEFAULT);
-void drawSquare(const size_t x, const size_t y, const size_t w, const size_t h, const Color color, const RenderRule rule = RenderRule::RENDER_DEFAULT);
-void drawBox(const size_t x, const size_t y, const size_t w, const size_t h, const Color color, const RenderRule rule = RenderRule::RENDER_DEFAULT);
-void drawCircle(const size_t x, const size_t y, const int r, const Color color, const RenderRule rule = RenderRule::RENDER_DEFAULT);
+void drawLine(const size_t x_0, const size_t y_0, const size_t x_1, const size_t y_1, const Color color, const RenderRule rule = RenderRule::RENDER_DEFAULT, void (*addToQueueService)(const Task&) = nullptr);
+void drawSquare(const size_t x, const size_t y, const size_t w, const size_t h, const Color color, const RenderRule rule = RenderRule::RENDER_DEFAULT, void (*addToQueueService)(const Task&) = nullptr);
+void drawBox(const size_t x, const size_t y, const size_t w, const size_t h, const Color color, const RenderRule rule = RenderRule::RENDER_DEFAULT, void (*addToQueueService)(const Task&) = nullptr);
+void drawCircle(const size_t x, const size_t y, const int r, const Color color, const RenderRule rule = RenderRule::RENDER_DEFAULT, void (*addToQueueService)(const Task&) = nullptr);
 
 #endif // DRAW_H
